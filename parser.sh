c2a2f835e9ecdf52dab02038713b4109ad1368b7
Lucian Lazar <lucian@mainquest.ro>
2021-11-17 21:19:19 +0200

    attempt to support windows via wsl

---
+++ parser.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+kf=$(mktemp)
+echo "$1" > "$kf"
+prefix="\"project_id\":"
+
+# Important to do this, as grep returns non-zero if no match is found
+echo "[Debug] Find project line in suppposedly non-encripted key"
+line=$(grep -F "$prefix" "$kf" || echo '')
+
+echo "[Debug] Test line"
+if [ -z "$line" ]; then
+    # We are dealing with a base64 string, probably
+    echo "[Debug] Decoding supposedly base64 key"
+    f=$(mktemp)
+
+    echo "[Debug] Decoding to $f"
+    (cat "$kf" | base64 -d > "$f") || echo "decoding failed"
+
+    echo "[Debug] Find project line in decripted key"
+    line=$(grep -F "$prefix" "$f" || echo '')
+    
+    echo "::set-output name=key_is_encrypted::true"
+else
+    echo "::set-output name=key_is_encrypted::false"
+fi
+
+if [ -z "$line" ]; then
+    echo "[Debug] We couldn\'t find \'$prefix\' string in the key file, nor in the decoded version of it. Trying without setting the project id..."
+else
+    echo "[Debug] Remove prefix, double quotes, columns and commas, and new lines from line"
+    project_id=$(echo "$line" | sed -e "s/$prefix//g" -e 's/\"//g' -e 's/\://g' -e 's/\,//g' | xargs echo -n)
+
+    echo "[Debug] Project id found: $project_id"
+    echo "::set-output name=project_id::$project_id"
+fi
