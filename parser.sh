eaeb73ae9a15e24d5004c2a77e3a0f80c3ecbc94
Lucian Lazar <lucian@mainquest.ro>
2022-12-10 11:31:26 +0200

    fixes

--- parser.sh
+++ parser.sh
@@ -3,7 +3,7 @@
 set -euox pipefail
 
 kf=$(mktemp)
-echo "$1" > "$kf"
+echo -e "$1" > "$kf"
 prefix="\"project_id\":"
 
 # Important to do this, as grep returns non-zero if no match is found
@@ -16,8 +16,11 @@ if [ -z "$line" ]; then
     echo "[Debug] Decoding supposedly base64 key"
     f=$(mktemp)
 
-    printf "[Debug] Replacing any potential literal '\\n' strings with an actual '\\n' char (LF): from %s to %s" "$kf" "$f"
-    sed 's/\\n/\n/g' "$kf" > "$f"
+    printf '[Debug] Replacing any potential "\\r\\n" strings (CRLF) with just "\\n" char (LF): from %s to %s' "$kf" "$f"
+    sed 's/\r\n/\n/g' "$kf" > "$f"
+
+    # printf '[Debug] Replacing any potential literal "\\n" strings with an actual \\n char (LF): from %s to %s' "$kf" "$f"
+    # sed 's/\\n/\n/g' "$kf" > "$f"
 
     f2=$(mktemp)
     echo "[Debug] Decoding $f to $f2"
